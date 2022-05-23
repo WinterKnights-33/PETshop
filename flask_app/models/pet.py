5c736bb20fb88f7258dba457375e6896f0f65e33
eligater <elie.r.knight@gmail.com>
2022-05-22 18:06:35 -0700

    Update pet.py

--- flask_app/models/pet.py
+++ flask_app/models/pet.py
@@ -1,4 +1,4 @@
-from Pet_Store_App.flask_app.models.user import User
+from flask_app.models.user import User
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app.models.user import user
 from flask import flash
