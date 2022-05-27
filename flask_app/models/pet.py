88d13b0a35314ed2e1d99408ceae333d0abc5b02
Mel-ware <86979895+Mel-ware@users.noreply.github.com>
2022-05-26 21:42:36 -0400

    Add files via upload

---
+++ flask_app/models/pet.py
@@ -0,0 +1,88 @@
+
+from flask_app.config.mysqlconnection import connectToMySQL
+
+from flask import flash
+
+class Pet:
+    db = "pet_shop_schema"
+    def __init__(self,data):
+        self.id = data['id']
+        self.name= data['name']
+        self.birthdate= data['birthdate']
+        self.description = data['description']
+        self.parents = data['parents']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.user_id = data['user_id']
+
+    
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO pets (name, birthdate, description, parents, user_id) VALUES (%(name)s, %(birthdate)s, %(description)s, %(parents)s, %(user_id)s);"
+
+        # comes back as the new row id
+        result = connectToMySQL('pet_shop_schema').query_db(query, data)
+        return result
+
+
+    @classmethod
+    def get_my_t(cls,data):
+        query = "SELECT first_name FROM users LEFT JOIN pets ON user.id = pet.user_id WHERE id = %(id)s;"
+        query = "SELECT * FROM pets WHERE user_id = %(id)s;"
+        results = connectToMySQL('pet_shop_schema').query_db(query,data)
+        petstoreturn = []
+        for result in results:
+            petstoreturn.append(cls(result))
+        return (petstoreturn)
+
+
+    @classmethod
+    def get_all_t(cls):
+        query = "SELECT first_name FROM users LEFT JOIN pets ON user.id = pet.user_id WHERE id = %(id)s;"
+        query = "SELECT * FROM pets;"
+        results = connectToMySQL('pet_shop_schema').query_db(query)
+        print (f"RESULTS{results}")
+        pets = []
+        for row in results:
+            pets.append( cls(row))
+        return pets
+
+
+    @classmethod
+    def get_one_t(cls,data):
+        query = "SELECT first_name FROM users LEFT JOIN pets ON user.id = pet.user_id WHERE id = %(id)s;"
+        query = "SELECT * FROM pets WHERE id = %(id)s;"
+        results = connectToMySQL('pet_shop_schema').query_db(query,data)
+        return cls(results[0])
+
+
+    @classmethod
+    def get_name(cls,data):
+        query = "SELECT * FROM users LEFT JOIN pets ON user.id = pet.user_id WHERE id = %(id)s;"
+        query = "SELECT first_name FROM users WHERE user_id = %(user_id)s;"
+        results = connectToMySQL('pet_shop_schema').query_db(query,data)
+        return cls(results[0])
+
+
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE pets SET name = %(name)s, birthdate = %(birthdate)s, description = %(description)s, parents = %(parents)s, user_id = %(user_id)s WHERE id = %(id)s;"
+        return connectToMySQL('pet_shop_schema').query_db(query, data)
+
+
+    @classmethod
+    def submit_report(cls, data):
+        query = "UPDATE pets SET birthdate = %(birthdate)s, description = %(description)s, user_id = %(user_id)s, created_by = %(created_by)s WHERE id = %(id)s;"
+        return connectToMySQL('pet_shop_schema').query_db(query, data)
+    
+
+    @classmethod
+    def report(cls, data):
+        query = "UPDATE pets SET birthdate = %(birthdate)s, description = %(description)s, created_by = %(created_by)s, WHERE id = %(id)s;"
+        return connectToMySQL('pet_shop_schema').query_db(query, data)
+
+
+    @classmethod
+    def destroy(cls, data):
+        query  = "DELETE FROM pets WHERE id = %(id)s;"
+        return connectToMySQL('pet_shop_schema').query_db(query, data)
---
+++ flask_app/static/js/script.js
@@ -0,0 +1,79 @@
+
+
+function red() {
+    var e = document.getElementById("cat-body");
+
+    if (!e) return true;
+    if (e.style.backgroundColor == "red") {
+        e.style.backgroundColor = ""
+    }
+    else {
+        e.style.backgroundColor = "red"
+    }
+    return true;
+}
+function pink() {
+    var e = document.getElementById("cat-body");
+
+    if (!e) return true;
+    if (e.style.backgroundColor == "pink") {
+        e.style.backgroundColor = ""
+    }
+    else {
+        e.style.backgroundColor = "pink"
+    }
+    return true;
+}
+function yellow() {
+    var e = document.getElementById("cat-body");
+
+    if (!e) return true;
+    if (e.style.backgroundColor == "yellow") {
+        e.style.backgroundColor = ""
+    }
+    else {
+        e.style.backgroundColor = "yellow"
+    }
+    return true;
+}
+
+
+/*var pet = {
+    'name' : "",
+    'hungry' : true,
+    'weight' : 0,
+    'age' : 0,
+    'photo' : "",
+};
+function feed(pet) {
+    if (pet.hungry === true) {
+    } else  {
+    console.log("I'm full. I already ate!");
+    }
+}
+function doSomething () {
+    alert("It's Done!");
+};
+doSomething();
+const Pet = prompt ("Name your pet");
+document.write(Action);
+let Energy = 0;
+let Happiness = 0;
+document.write(Energy);
+document.write(Happiness);
+for (i=0; i<6; i++){
+    var Action = prompt ("feed, pet, or walk?");
+    if (Action === "feed") {
+        Energy = (Energy + 2);
+    } else if (Action === "pet") {
+        Happiness = (Happiness + 1);
+    } else if (Action === "walk")
+    {
+        Happiness = ( Happiness + 2); 
+        Energy = (Energy - 1);
+    } 
+}; 
+console.log (Pet)
+console.log (Happiness)
+console.log (Energy)
+console.log(+ Pet + " has " + Happiness + " happiness and " + Energy + " energy");*/
